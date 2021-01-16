309af8625ef30960b8d5f6a5c4ef70531256e091
jian <577932965@qq.com>
2021-01-16 17:34:19 +0800

    vue.config.js

--- vue-admin-beautiful-master/vue.config.js
+++ vue-admin-beautiful-master/vue.config.js
@@ -3,19 +3,24 @@
  * @description cli配置
  */
 
+//vue cli4        https://github.com/staven630/vue-cli4-config
+//vue cli3        https://github.com/staven630/vue-cli4-config/tree/vue-cli3
+//vue.config.js   https://cli.vuejs.org/zh/config/#runtimecompiler
+//webpack         https://webpack.js.org/configuration/dev-server/#devservernoinfo-
+//webpackbar      https://www.npmjs.com/package/webpackbar    （个人理解应该是单页面加载的进度条）
 const path = require('path')
-const {
-  publicPath,
-  assetsDir,
-  outputDir,
-  lintOnSave,
-  transpileDependencies,
-  title,
-  abbreviation,
-  devPort,
-  providePlugin,
-  build7z,
-  donation,
+const {//引入部分配置变量
+  publicPath,// 公共路径(必须有的)
+  assetsDir,// 静态资源存放的文件夹(相对于ouputDir)
+  outputDir,// 输出文件目录
+  lintOnSave,// eslint-loader 是否在保存的时候检查(果断不用，这玩意儿我都没装)
+  transpileDependencies,//配置默认为[], babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来。配置需要转译的第三方库
+  title, //标题
+  abbreviation,//简写
+  devPort,//开发环境端口号
+  providePlugin,//webpack的内置模块，使用ProvidePlugin加载的模块在使用时将不再需要import和require进行引入，以jquery为例，用ProvidePlugin进行实例初始化后，jquery就会被自动加载并导入对应的node模块中
+  build7z,//npm run build时是否自动生成7z压缩包
+  donation,//是否显示终端donation打印
 } = require('./src/config')
 const { webpackBarName, webpackBanner, donationConsole } = require('zx-layouts')
 
@@ -40,32 +45,42 @@ const mockServer = () => {
 }
 
 module.exports = {
-  publicPath,
-  assetsDir,
-  outputDir,
-  lintOnSave,
-  transpileDependencies,
-  devServer: {
-    hot: true,
-    port: devPort,
-    open: true,
-    noInfo: false,
-    overlay: {
+  publicPath,// 公共路径(必须有的) / 默认'/'，部署应用包时的基本 URL
+  assetsDir,// 静态资源存放的文件夹(相对于ouputDir)
+  outputDir,// 输出文件目录
+  lintOnSave,// eslint-loader 是否在保存的时候检查(果断不用，这玩意儿我都没装)
+  transpileDependencies,// 进行编译的依赖
+  devServer: {//端口环境配置
+    proxy: {//代理地址
+      '/api': {
+        target: 'http:192.168.3.82:9001',
+        ws: true,
+        changeOrigin: true
+      }
+    },
+    hot: true,//单纯设置hot为true的时候，如果编译报错，会抛出错误，你重新改成正确的，这个时候又会触发重新编译，整个浏览器会重新刷新！
+    hotOnly: true,//在设置了hot:true，再将这个也设置为true的话，如果编译报错，你再改成正确的，重新编译，浏览器不会刷新！
+    port: devPort,//端口
+    open: true,// 是否打开浏览器
+    noInfo: false,//告诉开发服务器禁止显示诸如Webpack捆绑包信息之类的消息。错误和警告仍将显示。
+    host: "localhost",//ip地址
+    https: false,//是否https
+    overlay: {// 让浏览器 overlay 同时显示警告和错误
       warnings: true,
       errors: true,
     },
     after: mockServer(),
   },
-  configureWebpack() {
+  configureWebpack() {//绝对路径 和引入插件模块的配置
     return {
-      resolve: {
+      resolve: {//配置绝对路径
         alias: {
           '@': resolve('src'),
         },
       },
       plugins: [
-        new Webpack.ProvidePlugin(providePlugin),
-        new WebpackBar({
+        new Webpack.ProvidePlugin(providePlugin),//使用ProvidePlugin加载的模块  类似jQuery 配置引入
+        new WebpackBar({//需要npm安装的单页面路由进度条(支持多条进度条)
           name: webpackBarName,
         }),
       ],
@@ -162,8 +177,8 @@ module.exports = {
       })
     }
   },
-  runtimeCompiler: true,
-  productionSourceMap: false,
+  runtimeCompiler: true,// 是否使用包含运行时编译器的 Vue 构建版本
+  productionSourceMap: false,// 是否为生产环境构建生成 source map
   css: {
     requireModuleExtension: true,
     sourceMap: true,
